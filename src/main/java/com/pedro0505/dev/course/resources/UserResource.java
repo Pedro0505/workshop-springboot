80583117c705f0a84188d14fbbf3bfe69a0de0ea
pedro <pedrohrds05@hotmail.com>
2022-12-01 18:55:29 -0300

    feat: user entity

---
+++ src/main/java/com/pedro0505/dev/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.pedro0505.dev.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pedro0505.dev.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Pedro", "pedro@gmail.com", "9999999999", "1010110");
+		
+		return ResponseEntity.ok().body(u);
+	}
+}
