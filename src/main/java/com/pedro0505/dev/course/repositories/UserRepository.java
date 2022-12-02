fd3799d131281e22f60ca4b1ea8952fd0ea50624
Pedro Henrique <pedrohrds05@hotmail.com>
2022-12-02 10:36:16 -0300

    refactor: add a Repository decorator in User Repository

--- src/main/java/com/pedro0505/dev/course/repositories/UserRepository.java
+++ src/main/java/com/pedro0505/dev/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.pedro0505.dev.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.pedro0505.dev.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
