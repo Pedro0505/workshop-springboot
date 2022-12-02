339ad11fd0105c0d818bf274d4dcd4ef291963f6
Pedro Henrique <pedrohrds05@hotmail.com>
2022-12-02 12:06:18 -0300

    refactor: remove unuseless import

--- src/main/java/com/pedro0505/dev/course/repositories/OrderRepository.java
+++ src/main/java/com/pedro0505/dev/course/repositories/OrderRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.pedro0505.dev.course.entities.Order;
-import com.pedro0505.dev.course.entities.User;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
