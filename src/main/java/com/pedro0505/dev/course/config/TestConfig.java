487a5f7016bc8333064d8b2698d8b1928486a2b5
Pedro Henrique <pedrohrds05@hotmail.com>
2022-12-02 14:01:49 -0300

    feat: adding seeds in h2

--- src/main/java/com/pedro0505/dev/course/config/TestConfig.java
+++ src/main/java/com/pedro0505/dev/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.pedro0505.dev.course.entities.Category;
 import com.pedro0505.dev.course.entities.Order;
 import com.pedro0505.dev.course.entities.User;
 import com.pedro0505.dev.course.entities.enums.OrderStatus;
+import com.pedro0505.dev.course.repositories.CategoryRepository;
 import com.pedro0505.dev.course.repositories.OrderRepository;
 import com.pedro0505.dev.course.repositories.UserRepository;
 
@@ -24,8 +26,15 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
+
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
@@ -33,6 +42,7 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
+		this.categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		this.userRepository.saveAll(Arrays.asList(u1, u2));
 		this.orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
