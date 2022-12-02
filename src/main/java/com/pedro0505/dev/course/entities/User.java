fd140cf3c0185acc4c17fb6ba931917e4930297a
Pedro Henrique <pedrohrds05@hotmail.com>
2022-12-02 10:53:24 -0300

    feat: adding relation between order and users

--- src/main/java/com/pedro0505/dev/course/entities/User.java
+++ src/main/java/com/pedro0505/dev/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.pedro0505.dev.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,9 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {
 	}
 
@@ -32,6 +38,10 @@ public class User implements Serializable {
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	public Long getId() {
