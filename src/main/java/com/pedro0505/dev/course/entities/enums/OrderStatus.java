704da37b98e2ccb102216d9fd94082113927eda7
Pedro Henrique <pedrohrds05@hotmail.com>
2022-12-02 12:05:48 -0300

    feat: add order status enum

---
+++ src/main/java/com/pedro0505/dev/course/entities/enums/OrderStatus.java
@@ -0,0 +1,28 @@
+package com.pedro0505.dev.course.entities.enums;
+
+public enum OrderStatus {
+	WAITING_PAYMENT(1),
+	PAID(2),
+	SHIPPED(3),
+	DELIVERED(4),
+	CANCELED(5);
+	
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return this.code;
+	}
+	
+	public static OrderStatus valueOf(int code) {
+		for (OrderStatus value : OrderStatus.values()) {
+			if (value.getCode() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Invalid OrderStatus code");
+	}
+}
