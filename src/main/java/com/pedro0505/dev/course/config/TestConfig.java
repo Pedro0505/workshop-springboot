a87f0cb83f3bb092a8f3685e2867cdcba70472b7
Pedro Henrique <pedrohrds05@hotmail.com>
2022-12-02 19:59:56 -0300

    feat: adding order item seed

--- src/main/java/com/pedro0505/dev/course/config/TestConfig.java
+++ src/main/java/com/pedro0505/dev/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.pedro0505.dev.course.entities.Category;
 import com.pedro0505.dev.course.entities.Order;
+import com.pedro0505.dev.course.entities.OrderItem;
 import com.pedro0505.dev.course.entities.Product;
 import com.pedro0505.dev.course.entities.User;
 import com.pedro0505.dev.course.entities.enums.OrderStatus;
 import com.pedro0505.dev.course.repositories.CategoryRepository;
+import com.pedro0505.dev.course.repositories.OrderItemRepository;
 import com.pedro0505.dev.course.repositories.OrderRepository;
 import com.pedro0505.dev.course.repositories.ProductRepository;
 import com.pedro0505.dev.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -67,5 +72,12 @@ public class TestConfig implements CommandLineRunner {
 		
 		this.userRepository.saveAll(Arrays.asList(u1, u2));
 		this.orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		this.orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
