92755434422d69a8c547972d9ac40230bca4bec6
Pedro Henrique <pedrohrds05@hotmail.com>
2022-12-02 22:56:32 -0300

    feat: add payment entity

--- src/main/java/com/pedro0505/dev/course/config/TestConfig.java
+++ src/main/java/com/pedro0505/dev/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.pedro0505.dev.course.entities.Category;
 import com.pedro0505.dev.course.entities.Order;
 import com.pedro0505.dev.course.entities.OrderItem;
+import com.pedro0505.dev.course.entities.Payment;
 import com.pedro0505.dev.course.entities.Product;
 import com.pedro0505.dev.course.entities.User;
 import com.pedro0505.dev.course.entities.enums.OrderStatus;
@@ -79,5 +80,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		this.orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		
+		this.orderRepository.saveAll(Arrays.asList(o1));
 	}
 }
