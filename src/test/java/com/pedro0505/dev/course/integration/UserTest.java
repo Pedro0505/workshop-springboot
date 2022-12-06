d8c6d44decaa4d4131b1de3f7c95fd8bfa301fac
Pedro Henrique <pedrohrds05@hotmail.com>
2022-12-06 14:42:43 -0300

    test: create test to delete user

--- src/test/java/com/pedro0505/dev/course/integration/UserTest.java
+++ src/test/java/com/pedro0505/dev/course/integration/UserTest.java
@@ -1,12 +1,16 @@
 package com.pedro0505.dev.course.integration;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +23,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pedro0505.dev.course.entities.User;
 
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserTest {
 	@LocalServerPort
@@ -35,32 +40,40 @@ class UserTest {
 	}
 
 	@Test
+	@Order(1)
 	public void testPostUser() throws Exception {
-		User user = new User(1L, "Pedro", "pedro@gmail.com", "8198763242", "password");
+		User user = new User(null, "Pedro", "pedro@gmail.com", "8198763242", "password");
 		this.mockMvc
 				.perform(post(this.baseUrl).contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(user)))
-				.andExpect(status().is(HttpStatus.CREATED.value()))
-				.andExpect(jsonPath("$.name").value("Pedro"))
+				.andExpect(status().is(HttpStatus.CREATED.value())).andExpect(jsonPath("$.name").value("Pedro"))
 				.andExpect(jsonPath("$.email").value("pedro@gmail.com"))
-				.andExpect(jsonPath("$.phone").value("8198763242"))
-				.andExpect(jsonPath("$.password").value("password"));
+				.andExpect(jsonPath("$.phone").value("8198763242")).andExpect(jsonPath("$.password").value("password"));
 	}
 
 	@Test
+	@Order(2)
 	public void testGeAlltUser() throws Exception {
 		this.mockMvc.perform(get(this.baseUrl).contentType(MediaType.APPLICATION_JSON))
-				.andExpect(status().is(HttpStatus.OK.value()))
-				.andExpect(jsonPath("$[0].name").value("Maria Brown"))
-				.andExpect(jsonPath("$[1].name").value("Alex Green"))
-				.andExpect(jsonPath("$.length()").value(2));
+				.andExpect(status().is(HttpStatus.OK.value())).andExpect(jsonPath("$[0].name").value("Maria Brown"))
+				.andExpect(jsonPath("$[1].name").value("Alex Green")).andExpect(jsonPath("$.length()").value(3));
 	}
 
 	@Test
+	@Order(3)
 	public void testGetUserById() throws Exception {
 		this.mockMvc.perform(get(this.baseUrl.concat("/1")).contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().is(HttpStatus.OK.value())).andExpect(jsonPath("$.name").value("Maria Brown"))
 				.andExpect(jsonPath("$.email").value("maria@gmail.com"))
-				.andExpect(jsonPath("$.phone").value("988888888"))
-				.andExpect(jsonPath("$.password").value("123456"));
+				.andExpect(jsonPath("$.phone").value("988888888")).andExpect(jsonPath("$.password").value("123456"));
+	}
+
+	@Test
+	@Order(4)
+	public void testDeleteUser() throws Exception {
+		this.mockMvc.perform(delete(this.baseUrl.concat("/1")).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().is(HttpStatus.NO_CONTENT.value()));
+
+		this.mockMvc.perform(get(this.baseUrl.concat("/1")).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().is(HttpStatus.NOT_FOUND.value()));
 	}
 }
